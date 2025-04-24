46eb994cb9c065897d7996a8a44e0f83cfcf98fc
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei-2.local>
2025-04-24 12:49:02 -0600

    logout users

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from users.views import login
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', login, name='login'),
-    # path('logout/', logout, name='logout'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
 
