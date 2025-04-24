93a763236def96d3f279e346a4b79897388b7f45
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei-2.local>
2025-04-24 11:25:36 -0600

    CRUD pizzas

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from home.views import index, create_pizza, pizza_list
+from home.views import index, create_pizza, pizza_list, ModelDetailPizza, ViewUpdatePizza, ViewDeletePizza
 
 urlpatterns = [
     path('', index, name='index'),
      path('pizzas/', pizza_list, name='pizza_list'),
     path('pizzas/create/', create_pizza, name='create_pizza'),
+    # path('pizzas/<int:idPizza>/',detail_pizza , name='detail_pizza'),
+     path('pizzas/<int:pk>/', ModelDetailPizza.as_view(), name='detail_pizza'),
+    path('pizzas/<int:pk>/uptdate/', ViewUpdatePizza.as_view(), name='update_pizza'),
+    path('pizzas/<int:pk>/delete/', ViewDeletePizza.as_view() , name='delete_pizza'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from home.forms import PizzaForm
 from home.models import Pizza
+from django.views.generic import DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 def index(request):
     return render(request, 'home/index.html')
 
@@ -26,3 +28,23 @@ def pizza_list(request):
     pizzas = Pizza.objects.all()
     return render(request, 'home/pizza_list.html', {'pizzas': pizzas})
 
+def detail_pizza(request, idPizza):
+    pizza = Pizza.objects.get(id=idPizza)
+    return render(request, 'home/detail_pizza.html', {'pizza': pizza})
+
+class ModelDetailPizza(DetailView):
+    model = Pizza
+    template_name = 'home/detail_pizza.html'
+
+class ViewUpdatePizza(UpdateView):
+    model = Pizza
+    template_name = 'home/update_pizza.html'
+    fields = ['name', 'size', 'price', 'date_created']
+    success_url = reverse_lazy('pizza_list')
+
+class ViewDeletePizza(DeleteView):
+    model = Pizza
+    template_name = 'home/delete_pizza.html'
+    success_url = reverse_lazy('pizza_list')
+
+    
